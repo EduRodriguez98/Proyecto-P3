2e512c2da6b398c913054ee342943dbe8f70ef8c
AdolfoCesar <45486593+AdolfoCesar@users.noreply.github.com>
2019-09-20 23:40:52 +0200

    Update Eduardo.java

--- Eduardo.java
+++ Eduardo.java
@@ -1,4 +1,11 @@
 
 public class Eduardo {
 //hola, estoy probando cosas
-}
+  //creo que no tengo acceso para escribir o no se que pero puedo semi editar o algo asi. 
+  
+  for(queGuaposoy:queTipitoTengo){
+    do{
+      sex
+}while(hotGirl);
+    
+    
