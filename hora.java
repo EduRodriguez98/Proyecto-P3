930305628cfaf566adb51689bf588c99de5403a9
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2019-09-24 11:17:35 +0200

    programa que te lee la hora y la fecha

---
+++ hora.java
@@ -0,0 +1,21 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class hora {
+	
+	public static void hora() {
+		Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println( sdf.format(cal.getTime()) );
+	}
+	
+	
+
+	public static void main(String[] args) {
+
+		Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        System.out.println( sdf.format(cal.getTime()) );
+	}
+
+}
