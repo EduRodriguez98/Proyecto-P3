775e21e62e93cffa7d9072047fa9de6f3c0c4476
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2019-10-23 21:06:31 +0200

    Para elegir foto

---
+++ src/proyectPackage/FileChooserDemo.java
@@ -0,0 +1,18 @@
+package proyectPackage;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileChooserDemo {
+    public static void main(String[] args){
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "JPG & GIF Images", "jpg", "gif");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(null);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " +
+                    chooser.getSelectedFile().getName());
+        }
+    }
+}
