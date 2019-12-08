616a994e94391eeab8ce75162a41dc595231db52
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2019-12-08 21:13:13 +0100

    antes del metodo de internet

--- src/conexion/BaseDatosModise.java
+++ src/conexion/BaseDatosModise.java
@@ -1,13 +1,8 @@
 package conexion;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -58,47 +53,6 @@ public class BaseDatosModise {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private byte[] readFile(String file) {
-		ByteArrayOutputStream bos = null;
-		try {
-			File f = new File(file);
-			FileInputStream fis = new FileInputStream(f);
-			byte[] buffer = new byte[1024];
-			bos = new ByteArrayOutputStream();
-			for (int len; (len = fis.read(buffer)) != -1;) {
-				bos.write(buffer, 0, len);
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());
-		} catch (IOException e2) {
-			System.err.println(e2.getMessage());
-		}
-		return bos != null ? bos.toByteArray() : null;
-	}
-
-	public void updatePicture(int materialId, String filename) {
-		// update sql
-		String updateSQL = "insert into pruebaimagenes(imagen, id) values(" + "imagen = ? " + "id = ?)";
-
-		try (Connection conn = conectar(); PreparedStatement pstmt = conn.prepareStatement(updateSQL)) {
-
-			// set parameters
-			pstmt.setBytes(1, readFile(filename));
-			pstmt.setInt(2, materialId);
-
-			pstmt.executeUpdate();
-			System.out.println("Stored the file in the BLOB column.");
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public static void main(String[] args) {
-		BaseDatosModise app = new BaseDatosModise();
-		app.updatePicture(99, "C:\\Users\\Eneko Pérez\\Pictures\\wp.jpg");
 	}
 
 	/*
